ea122cb4dd9eaf8d858156d39ff359c2a8054506
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-03-25 03:04:16 +0100

    ComponenteHome

--- src/pages/Registro.tsx
+++ src/pages/Registro.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Registro: React.FC = () => {
   return (
     <div>
       <h1>Pantalla de registro</h1>
       <p>Esta es la página de registro.</p>
+      <Link to="/">Voltar para Home</Link>
     </div>
   );
 };
